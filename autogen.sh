2836e64645a68d3a7c4f68afe76c01168bdda8b2
Thorsten Kukuk <kukuk@suse.com>
2020-07-06 10:51:55 +0200

    Make sure config.rpath get's updated, remove others from git

--- autogen.sh
+++ autogen.sh
@@ -1,6 +1,8 @@
 #!/bin/sh -x
 
 rm -fv ltmain.sh config.sub config.guess config.h.in
+# Update config.rpath, autotools are not doing this
+test -f /usr/share/gettext/config.rpath && cp /usr/share/gettext/config.rpath .
 mkdir -p m4
 aclocal -I m4
 autoheader
